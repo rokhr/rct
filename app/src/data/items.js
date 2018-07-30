12a951911ed7316db8071c2257ef13eac699f42c
rok <valid_email@example.com>
2018-07-30 23:25:21 +0900

    all inited

---
+++ app/src/data/items.js
@@ -0,0 +1,19 @@
+const items =[
+  {
+    title: 'itemA',
+    description: 'まずはAのアイテムです。',
+    tags: ['']
+  },
+  {
+    title: 'itemB',
+    description: 'Bです。Aとは〇〇が異なります。',
+    tags: ['']
+  },
+  {
+    title: 'itemC',
+    description: 'Cです。Aに〇〇を付け加えたものです。',
+    tags: ['']
+  }
+];
+
+export default items;
---
+++ app/src/views/components/item.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from "react";
+
+class Items extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="app-item">
+        <p class="app-item__title">タイトル: {this.props.title}</p>
+        <p class="app-item__description">
+          説明:{this.props.description}
+        </p>
+      </div>
+    );
+  }
+}
+export default Items;
