026c40b0cd5859bb5c1fcf7a887892f579c9f60b
rok <valid_email@example.com>
2018-08-01 07:53:58 +0900

    vvv

--- app/src/views/components/tabBox/tabBox.js
+++ app/src/views/components/tabBox/tabBox.js
@@ -12,22 +12,38 @@ const data = [
   }
 ]
 
-class tabBox extends PureComponent {
+class tabBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: 0
+    }
+  }
+
+  onClick(event, index) {
+    console.log(index, event);
+    this.setState({
+      selected: index,
+    })
   }
 
   render() {
     return (
       <div class="app-tabPanels">
         <ul class="app-tabPanels__tabList">
-          {data.map((data) => {
-            return <li class="app-tabPanels__tabItem">{data.tabName}</li>;
-          })}
+          {data.map((data, index) => 
+            <li key={index} class="app-tabPanels__tabItem" onClick={(e) => this.onClick(e, index)}>
+              {data.tabName}
+            </li>
+          )}
         </ul>
         <div class="app-tabPanels__panelList">
-          {data.map((data) => {
-            return <div class="app-tabPanels__panelItem">{data.tabContents}</div>;
+          {data.map((data, index) => {
+            return (
+              <div class={index === this.state.selected ? 'app-tabPanels__panelItem--active' : 'app-tabPanels__panelItem'}>
+                {data.tabContents}
+              </div>
+            );
           })}
         </div>
       </div>
